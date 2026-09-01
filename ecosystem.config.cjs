64287226315aa0a97957d12e2b61697b450c97a0
Ravi Warrier <42061703+raviwarrier@users.noreply.github.com>
2026-09-01 11:08:14 +0000

    Update ecosystem.config.cjs for app configuration
    Changed execution mode to 'fork' and added cwd path.
    Fixed the non working ecosystem.config.js file.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'is-it-pink',
-      script: './dist/server.cjs',
-      instances: 'max',
-      exec_mode: 'cluster',
+      name: "is-it-pink",
+      cwd: "/PATH/TO/is-it-pink", //remember to add instructions for this to readme.md as well 
+      script: "./dist/server.cjs",
+      exec_mode: "fork",
       env: {
-        NODE_ENV: 'production',
-        PORT: 4260
+        NODE_ENV: "production",
+        //HOST: "192.168.68.102" //OPTIONAL IF YOU WANT TO BIND IT
       }
     }
   ]
